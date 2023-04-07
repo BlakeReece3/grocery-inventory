a3a0487252eb2966e5625cd460af1edb854c11f2
Blake Reece <frankiereece3@gmail.com>
2023-04-06 19:17:59 -0700

    added init function for food items,added index.js properties

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Food = require('./Food')
+
+User.hasMany(Food, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Food.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Food };
