2b5bdaaebcef98b573a3ece296100096f6ac6438
Blake Reece <frankiereece3@gmail.com>
2023-04-10 20:55:34 -0700

    added homepage.handlesbars

--- models/index.js
+++ models/index.js
@@ -12,4 +12,13 @@ Need.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
+User.hasMany(Inventory, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Inventory.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
 module.exports = { User, Need, Inventory };
