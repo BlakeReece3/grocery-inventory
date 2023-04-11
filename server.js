af238e9990b11f7a3260fa1e54020ed4e2c57ad2
agcontr1 <alexander.ga.contreras@gmail.com>
2023-04-10 18:53:52 -0700

    html

--- server.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const app = express();
-const PORT = process.env.PORT || 3001;
